813db70099cab5de9cce1ac340a84e459fc89469
AvocatViorel <avocatialex2012@gmail.com>
2026-03-04 01:14:41 +0200

    Delete functions directory

--- functions/api/countries.js
+++
@@ -1,8 +0,0 @@
-import {DEFAULT_COUNTRIES} from '../defaults.js';
-export async function onRequestGet({env}) {
-  const kv = env.CONTENT;
-  let data = null;
-  try { data = await kv.get('countries', {type:'json'}); } catch {}
-  if (!Array.isArray(data) || !data.length) data = DEFAULT_COUNTRIES;
-  return new Response(JSON.stringify(data), {headers:{'content-type':'application/json; charset=utf-8'}});
-}
--- functions/api/reset.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost({request, env}) {
-  const kv = env.CONTENT;
-  const body = await request.json().catch(()=>({}));
-  if (!body.confirm) return new Response('confirm required', {status:400});
-  await kv.delete('site');
-  await kv.delete('countries');
-  await kv.delete('articles');
-  return new Response(JSON.stringify({ok:true}), {headers:{'content-type':'application/json; charset=utf-8'}});
-}
